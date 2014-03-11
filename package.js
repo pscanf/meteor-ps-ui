fbd5489993a906d2836af70e3283698cca63293a
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-03-11 19:10:55 +0100

    Updated ps-ui, including now non-minified files. Changed version number to 0.1.1 to match ps-ui's version. Fixed dependencies in smart.json. Fixed package.js.

--- package.js
+++ package.js
@@ -3,11 +3,14 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+	// dependencies
     api.use("ngMeteor", "client");
+	api.use("angular-bootstrap", "client");
+	api.use("angular-ui-utils", "client");
 
     // ps-ui files.
-    api.add_files("lib/ps-ui-tpls.min.js", "client");
-    api.add_files("lib/ps-ui.min.css", "client");
+    api.add_files("lib/ps-ui-tpls.js", "client");
+    api.add_files("lib/ps-ui.css", "client");
 
     // Client files.
     api.add_files("init.js", "client");
